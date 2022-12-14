9dfc13bf4d12039effe740463f070875542ded2f
m.tlon <marcin.tlon1@gmail.com>
2022-12-14 17:55:31 +0100

    Data access object created

---
+++ src/main/java/com/springSecurity/SecurityApplication/auth/ApplicationUserDAO.java
@@ -0,0 +1,7 @@
+package com.springSecurity.SecurityApplication.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDAO {
+    Optional <ApplicationUser> selectApplicationUserByUsername(String username);
+}
---
+++ src/main/java/com/springSecurity/SecurityApplication/auth/ApplicationUserService.java
@@ -0,0 +1,20 @@
+package com.springSecurity.SecurityApplication.auth;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ApplicationUserService implements UserDetailsService {
+    private final ApplicationUserDAO applicationUserDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserDAO
+                .selectApplicationUserByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException(String.format("Username not fount",username)));
+    }
+}
